01fa889aafd91735cfe279938c5795eed3abff2f
u-toru42 <golt7040@gmail.com>
2022-11-16 17:41:58 +0900

    profile間でfollow関係をまとめる

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,28 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.find_by(profile_params[:id]) 
+    # プロファイル全般
+    @user = User.find_by(profile_params[:id])
+    # フォロー関係
+    @events = @user.events.page(params[:page]).reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
+  end
+
+  def follows
+    user = User.find_by(params[:id])
+    @users = user.following_user.page(params[:page]).reverse_order
+  end
+
+  def followers
+    user = User.find_by(params[:id])
+    @users = user.follower_user.page(params[:page]).reverse_order
+  end
+
+  def show_follow
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def profile_params
