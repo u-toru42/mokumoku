99df44cf960a6247274a6fb6cd886864fb88dd39
u-toru42 <golt7040@gmail.com>
2022-11-16 20:13:30 +0900

    遷移先のidの反映バグ修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     # プロファイル全般
-    @user = User.find_by(profile_params[:id])
+    @user = User.find_by(id: params[:id])
     # フォロー関係
     @events = @user.events.page(params[:page]).reverse_order
     @following_users = @user.following_user
@@ -9,23 +9,20 @@ class ProfilesController < ApplicationController
   end
 
   def follows
-    user = User.find_by(params[:id])
+    user = User.find_by(id: params[:id])
     @users = user.following_user.page(params[:page]).reverse_order
   end
 
   def followers
-    user = User.find_by(params[:id])
+    user = User.find_by(id: params[:id])
     @users = user.follower_user.page(params[:page]).reverse_order
   end
 
   def show_follow
-    @user = User.find(params[:id])
+    @user = User.find(id: params[:id])
     @posts = @user.posts.page(params[:page]).reverse_order
     @following_users = @user.following_user
     @follower_users = @user.follower_user
   end
 
-  def profile_params
-    params.permit(:id)
-  end
 end
